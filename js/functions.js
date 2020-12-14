95fe31be189c7cc050e5403cee110793dcdde446
Andre Ofsoski <andre@initio.co.nz>
2020-12-14 21:35:20 +1300

    master - add entire project - product landing page - taco time
    https://www.freecodecamp.org/learn/responsive-web-design/responsive-web-design-projects/build-a-product-landing-page

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+//Could add a responsive burger here - would be simple with bulma/bootstrap
+// const element = document.querySelector('.navbar');
+// const style = getComputedStyle(element);
+
+// function navBurger() {
+//     var x = document.getElementById("nav-link");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+// }
